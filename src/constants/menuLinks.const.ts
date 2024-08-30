580595d8d022c14031f41a4fe20217e614ca969b
Mahfuz <aamahfuz.pro@gmail.com>
2024-08-30 23:02:59 +0600

    footer-done with responsive

--- src/constants/menuLinks.const.ts
+++ src/constants/menuLinks.const.ts
@@ -20,3 +20,11 @@ export const menuLinks = [
     path: "/",
   },
 ];
+// Footer Menu Links
+export const categories = [
+  { name: "Tour", path: "/tour" },
+  { name: "Travel", path: "/travel" },
+  { name: "Group Tour", path: "/group-tour" },
+  { name: "Collage Tour", path: "/collage-tour" },
+  { name: "Air Travel ", path: "/air-travel" },
+];
