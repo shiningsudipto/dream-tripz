869d8130ee031bdbe0012c3b4101d2e5a54b44ec
Sudipta Das <shiningsudipto@gmail.com>
2024-09-06 12:36:46 +0600

    logo and menu links constant added

---
+++ src/constants/menuLinks.const.ts
@@ -0,0 +1,22 @@
+const menuLinks = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Destination",
+    path: "/",
+  },
+  {
+    name: "Tour Package",
+    path: "/",
+  },
+  {
+    name: "Blogs",
+    path: "/",
+  },
+  {
+    name: "Contact Us",
+    path: "/",
+  },
+];
