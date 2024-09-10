b3e3002e7a7d22cf08daaa7cb5aa8847c67173d0
Mahfuz <aamahfuz.pro@gmail.com>
2024-09-11 02:08:25 +0600

    added framer motion in featured sectons

--- src/components/Sections/featured-blog.tsx
+++ src/components/Sections/featured-blog.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useState } from "react";
 import {
@@ -8,6 +9,7 @@ import {
   FaMapMarkerAlt,
 } from "react-icons/fa";
 
+// Blog Data
 const blogs = [
   {
     id: 1,
@@ -86,14 +88,33 @@ const FeaturedBlogSection = () => {
 
   const isExpanded = (id: number) => expandedIds.includes(id);
 
+  const variants = {
+    enter: (direction: string) => ({
+      opacity: 0,
+      x: direction === "next" ? 100 : -100,
+      scale: 0.1,
+    }),
+    center: {
+      opacity: 1,
+      x: 0,
+      scale: 1,
+    },
+    exit: (direction: string) => ({
+      opacity: 0,
+      x: direction === "next" ? -100 : 100,
+      scale: 0.8,
+    }),
+  };
+
   return (
-    <section className="container mx-auto py-10 px-4  mt-[190px]">
+    <section className="container mx-auto py-10 px-4 mt-[190px]">
       {/* Header Section */}
       <div className="flex justify-between items-center mb-8">
         <h2 className="text-2xl font-bold">Featured Blog Posts</h2>
         <div className="flex space-x-4">
           <button
             onClick={handlePrev}
+            aria-label="Previous Blog Post"
             className={`text-gray-600 rounded-full p-2 transition-all ${
               activeButton === "prev"
                 ? "bg-[#003349] text-white"
@@ -104,6 +125,7 @@ const FeaturedBlogSection = () => {
           </button>
           <button
             onClick={handleNext}
+            aria-label="Next Blog Post"
             className={`text-gray-600 rounded-full p-2 transition-all ${
               activeButton === "next"
                 ? "bg-[#003349] text-white"
@@ -115,73 +137,94 @@ const FeaturedBlogSection = () => {
         </div>
       </div>
 
-      {/* Blog Cards */}
-      <div className="flex gap-6 overflow-hidden transition-all duration-300 ease-in-out">
-        {blogs.slice(currentIndex, currentIndex + 3).map((blog) => (
-          <div
-            key={blog.id}
-            className="w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.333%-1rem)] bg-white shadow-md rounded-lg p-4 my-2 flex-shrink-0"
-          >
-            <Image
-              src={blog.image}
-              alt={blog.title}
-              width={400}
-              height={200}
-              className="w-full h-48 object-cover rounded-t-lg"
-            />
-            <div className="flex items-center justify-between mt-4 text-sm text-gray-500">
-              <div className="flex items-center text-red-600">
-                <FaMapMarkerAlt className="mr-1" />
-                {blog.location}
-              </div>
-              <div className="flex items-center text-gray-700">
-                <FaComments className="mr-1" />
-                Comments {blog.comments}
-              </div>
-            </div>
-            <h3 className="mt-4 font-semibold text-lg">{blog.title}</h3>
+      {/* Blog Cards with Animation */}
+      {blogs.length > 0 ? (
+        <div className="flex gap-6 overflow-hidden">
+          <AnimatePresence initial={false} custom={activeButton}>
+            {blogs.slice(currentIndex, currentIndex + 3).map((blog) => (
+              <motion.div
+                key={blog.id}
+                custom={activeButton}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                  damping: 50,
+                  mass: 1,
+                  duration: 0.5, // Duration of the transition
+                }}
+                whileHover={{ scale: 1.09 }}
+                className="w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.333%-1rem)] bg-white shadow-md rounded-lg p-4 my-2 flex-shrink-0 transition-transform duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl"
+              >
+                <Image
+                  src={blog.image}
+                  alt={`Image of ${blog.title}`}
+                  width={400}
+                  height={200}
+                  className="w-full h-48 object-cover rounded-t-lg"
+                />
+                <div className="flex items-center justify-between mt-4 text-sm text-gray-500">
+                  <div className="flex items-center text-red-600">
+                    <FaMapMarkerAlt className="mr-1" />
+                    {blog.location}
+                  </div>
+                  <div className="flex items-center text-gray-700">
+                    <FaComments className="mr-1" />
+                    Comments {blog.comments}
+                  </div>
+                </div>
+                <h3 className="mt-4 font-semibold text-lg">{blog.title}</h3>
 
-            {/* Content with Read More/Read Less functionality */}
-            <p className="text-gray-600 mt-2 text-sm">
-              {isExpanded(blog.id) ? (
-                <>
-                  {blog.content}
-                  <span
-                    className="text-gray-600 cursor-pointer ml-2"
-                    onClick={() => toggleReadMore(blog.id)}
-                  >
-                    Read less
-                  </span>
-                </>
-              ) : (
-                <>
-                  {blog.content.slice(0, 100)}{" "}
-                  <span
-                    className="text-gray-600 cursor-pointer"
-                    onClick={() => toggleReadMore(blog.id)}
-                  >
-                    Read more...
-                  </span>
-                </>
-              )}
-            </p>
+                {/* Content with Read More/Read Less functionality */}
+                <p className="text-gray-600 mt-2 text-sm">
+                  {isExpanded(blog.id) ? (
+                    <>
+                      {blog.content}
+                      <span
+                        className="text-gray-600 cursor-pointer ml-2"
+                        onClick={() => toggleReadMore(blog.id)}
+                      >
+                        Read less
+                      </span>
+                    </>
+                  ) : (
+                    <>
+                      {blog.content.slice(0, 100)}{" "}
+                      <span
+                        className="text-gray-600 cursor-pointer"
+                        onClick={() => toggleReadMore(blog.id)}
+                      >
+                        Read more...
+                      </span>
+                    </>
+                  )}
+                </p>
 
-            <div className="flex items-center mt-4">
-              <Image
-                src={blog.profileImg}
-                alt={blog.author}
-                width={30}
-                height={30}
-                className="rounded-full"
-              />
-              <div className="ml-3 text-sm text-gray-900">
-                <p className="font-semibold">{blog.author}</p>
-                <p className="">{blog.date}</p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+                <div className="flex items-center mt-4">
+                  <Image
+                    src={blog.profileImg}
+                    alt={`Profile image of ${blog.author}`}
+                    width={30}
+                    height={30}
+                    className="rounded-full"
+                  />
+                  <div className="ml-3 text-sm text-gray-900">
+                    <p className="font-semibold">{blog.author}</p>
+                    <p className="">{blog.date}</p>
+                  </div>
+                </div>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+      ) : (
+        <p className="text-center text-gray-600">
+          No blogs available at the moment.
+        </p>
+      )}
     </section>
   );
 };
