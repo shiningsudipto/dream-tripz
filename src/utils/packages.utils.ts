ccc43af8abaf1213da1989c80097b808eb28062d
Sudipta Das <shiningsudipto@gmail.com>
2024-09-15 23:00:06 +0600

    packages - array and images added

---
+++ src/utils/packages.utils.ts
@@ -0,0 +1,68 @@
+export const travelPackages = [
+  {
+    title: "Kuala Lumpur with Genting Highland",
+    duration: "5D/6N",
+    people: 4,
+    location: "Kuala Lumpur",
+    price: 1200,
+    originalPrice: 1450,
+    image: "/images/tour-packages/Kuala-Lumpur.jpg",
+    description:
+      "After an early breakfast, check out from Hotel & leave for an overnight stay at Pahalgam.",
+  },
+  {
+    title: "Enforced Disappearances in Kashmir: A State's Story",
+    duration: "8D/9N",
+    people: 6,
+    location: "Kashmir",
+    price: 1400,
+    originalPrice: 1650,
+    image: "/images/tour-packages/Kashmir.jpg",
+    description:
+      "Upon arrival at Srinagar Sheikh Ul Alam Int'l Airport, this must be one of the most scenic flights.",
+  },
+  {
+    title: "Sunset View Of Beautiful Lakeside Resident",
+    duration: "7D/8N",
+    people: 5,
+    location: "Singapore",
+    price: 1100,
+    originalPrice: 1250,
+    image: "/images/tour-packages/Singapore.jpg",
+    description:
+      "After an early breakfast, check out from Hotel & leave for an overnight stay at Pahalgam.",
+  },
+  {
+    title: "The Majestic Mountains of Switzerland",
+    duration: "6D/7N",
+    people: 3,
+    location: "Switzerland",
+    price: 2200,
+    originalPrice: 2500,
+    image: "/images/tour-packages/Switzerland.jpg",
+    description:
+      "Experience the breathtaking views and scenic routes of the Swiss Alps.",
+  },
+  {
+    title: "A Serene Journey through Kyoto's Temples",
+    duration: "4D/5N",
+    people: 2,
+    location: "Kyoto, Japan",
+    price: 1800,
+    originalPrice: 2000,
+    image: "/images/tour-packages/Kyoto-Japan.jpg",
+    description:
+      "Explore the beautiful temples of Kyoto and enjoy a peaceful retreat.",
+  },
+  {
+    title: "Adventure in the Australian Outback",
+    duration: "10D/11N",
+    people: 6,
+    location: "Australia",
+    price: 2500,
+    originalPrice: 2800,
+    image: "/images/tour-packages/Australia.jpg",
+    description:
+      "Embark on a thrilling adventure through Australia's rugged outback landscapes.",
+  },
+];
